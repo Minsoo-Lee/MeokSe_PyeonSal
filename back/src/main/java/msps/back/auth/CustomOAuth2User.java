b645308cc4422faf477d043c0287c03f85771ffb
이민수 <minsoo.james1101@gmail.com>
2026-08-28 15:56:10 +0900

    refactor: CustomOAuth2User에서 안 쓰는 것들 정리

--- back/src/main/java/msps/back/auth/CustomOAuth2User.java
+++ back/src/main/java/msps/back/auth/CustomOAuth2User.java
@@ -1,23 +1,15 @@
 package msps.back.auth;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
-import msps.back.dto.request.UserEditRequest;
-import msps.back.dto.request.UserSignupRequest;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @Getter
-@Setter
 @ToString
 public class CustomOAuth2User extends DefaultOidcUser {
 
---
+++ back/src/main/java/msps/back/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package msps.back.repository;
+
+public class UserRepository {
+}
